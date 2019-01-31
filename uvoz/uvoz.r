18d5afa8316959118a8d40c5a652165d8b65ca22
Saša Prelog <sasa.prelog6@gmail.com>
2019-01-31 15:57:45 +0100

    popravljena imena spremenljivk in ostalega, da ne vsebujejo šumnikov in presledkov itd., narejenih prvih nekaj grafov

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -5,26 +5,34 @@ sl <- locale("sl", decimal_mark=",", grouping_mark=".")
 
 #Funkcija, ki uvozi podatke iz datoteke T1kolicine_locenih_odpadkov.csv
 data1 <- read_csv2("podatki/T1kolicine_locenih_odpadkov.csv", n_max=7, skip=5, na="-", 
-                  col_names=c("vrsta odpadkov", 2002:2017),
+                  col_names=c("vrsta_odpadkov", 2002:2017),
                     locale=locale(encoding="Windows-1250"))
 
-vrsta.odpadkov <- melt(data1, id.vars="vrsta odpadkov", measure.vars=names(data1)[-1],
+data1$vrsta_odpadkov <- c("skupaj", "embalaza", "papir", "steklo", "kuhinjski_odpadki", "plastika", "drugo")
+
+vrsta.odpadkov <- melt(data1, id.vars="vrsta_odpadkov", measure.vars=names(data1)[-1],
                        variable.name="leto", value.name="tone", na.rm=TRUE)
 
 #Funkcija, ki uvozi podatke iz datoteke T2predelava odpadkov(tone).csv
 data2 <- read_csv2("podatki/T2predelava odpadkov(tone).csv", skip=5, n_max=9, na=c("-","..."),
-                   col_names=c("zbriš","način predelave",2002:2017),
+                   col_names=c("zbris","nacin_predelave",2002:2017),
                    locale=locale(encoding="Windows-1250"))
 
-data2 <- data2[,c("način predelave",2002:2017)]
+data2 <- data2[,c("nacin_predelave",2002:2017)]
 
-predelava.odpadkov <- melt(data2, id.vars="način predelave", measure.vars=names(data2)[-1],
+predelava.odpadkov <- melt(data2, id.vars="nacin_predelave", measure.vars=names(data2)[-1],
                           variable.name="leto", value.name="tone", na.rm=TRUE)
 
 #Funkcija, ki uvozi podatke iz datoteke T3nastali odpadki po regijah(tone).csv
 data3 <- read_csv2("podatki/T3nastali odpadki po regijah(tone).csv", skip=5, n_max=15, 
                    col_names=c("regije", 2010:2017),
                    locale=locale(encoding="Windows-1250"))
+
+data3 <- data3[c(1,3:9,12,13,10,14,15,FALSE,FALSE),]
+
+regije <- c("Slovenija", "Pomurska", "Podravska", "Koroska", "Savinjska", "Zasavska", "Posavska", "Jugovzhodna_Slovenija", "Osrednjeslovenska", "Gorenjska", "Primorsko-notranjska", "Goriska", "Obalno-kraska")
+
+data3$regije <- regije
 
 nastali.odpadki.regije <- melt(data3, id.vars="regije", measure.vars=names(data3)[-1],
                                variable.name="leto", value.name="tone", na.rm=TRUE)
@@ -34,6 +42,9 @@ data5 <- read_delim("podatki/T4procent loceno zbranih odpadkov po regijah.csv", 
                    col_names=c("regije", 2002:2017),
                   locale=locale(encoding="Windows-1250",decimal_mark="."))
 
+data5 <- data5[c(1,3:9,12,13,10,14,15,FALSE,FALSE),]
+data5$regije <- regije
+
 loceni.odpadki.regije <- melt(data5, id.vars="regije", measure.vars=names(data5)[-1],
                               variable.name="leto", value.name="delez", na.rm=TRUE)
 
@@ -42,8 +53,10 @@ data6 <- read_csv2("podatki/T5investicije po regijah (1000eur).csv", skip=4, n_m
                    col_names=c("regije", 2001:2016),
                    locale=locale(encoding="Windows-1250"))
 
+data6$regije <- regije
+
 investicije.regije1 <- melt(data6, id.vars="regije", measure.vars=names(data6)[-1],
-                          variable.name="leto", value.name="1000 EUR", na.rm=TRUE)
+                          variable.name="leto", value.name="1000_EUR", na.rm=TRUE)
 
 
 #Funkcija, ki uvozi podatke iz datoteke T5investicije za varstvo okolja po regijah(indeksi regionalnega bdp).csv
@@ -52,17 +65,18 @@ data7 <- read_delim("podatki/T5investicije za varstvo okolja po regijah(indeksi 
                     locale=locale(encoding="Windows-1250",decimal_mark="."))
 
 data7[9:11,] <- data7[c(10,11,9),]
+data7$regije <- regije
 
 investicije.regije2 <- melt(data7, id.vars="regije", measure.vars=names(data7)[-1],
-                            variable.name="leto", value.name="% regionalnega BDP", na.rm=TRUE)
+                            variable.name="leto", value.name="delez_regionalnega_BDP", na.rm=TRUE)
 
 #Funkcija, ki združi podatke o investicijah v eno tabelo
 investicije.regije = data.frame(investicije.regije1, investicije.regije2[3])
-names(investicije.regije) = c(names(investicije.regije1), "% regionalnega BDP")
+names(investicije.regije) = c(names(investicije.regije1), "delez_regionalnega_BDP")
 
 #Funkcija, ki uvozi podatke iz datoteke T6srednja po regijah.csv
 data8 <- read_csv2("podatki/T6srednja po regijah.csv", skip=3, n_max=13, 
-                   col_names=c("regije", "srednja izobrazba"),
+                   col_names=c("regije", "srednja_izobrazba"),
                    locale=locale(encoding="Windows-1250"))
 
 #Funkcija, ki uvozi podatke iz datoteke T6visja po regijah.csv
@@ -70,13 +84,15 @@ data9 <- read_csv2("podatki/T6visja po regijah.csv", skip=3, n_max=13,
                    col_names=c("regije", 1:3 ),
                    locale=locale(encoding="Windows-1250"))
 
-data9["visja izobrazba"] <- apply(data9[-1], 1, sum)
+data9["visja_izobrazba"] <- apply(data9[-1], 1, sum)
 
 
 #Funkcija, ki uvozi podatke iz datoteke T6osnovnosolska po regijah.csv
 data10 <- read_csv2("podatki/T6osnovnosolska po regijah.csv", skip=3, n_max=13, 
                    col_names=c("regije","osnovnosolska"),
                    locale=locale(encoding="Windows-1250"))
+
+data10$regije <- regije
 
 #Funkcija, ki združi podatke o izobrazbi v eno tabelo
 izobrazba.regije <- data.frame(data10, data8[2], data9[5])
@@ -88,6 +104,7 @@ data11 <- read_csv2("podatki/T7otroci.csv", skip=5, n_max=13,
                     locale=locale(encoding="Windows-1250"))
 
 data11 <- data11[,c("regije",2002:2017)]
+data11$regije <- regije
 
 starostne.skupine.regije1 <- melt(data11, id.vars="regije", measure.vars=names(data11)[-1],
                               variable.name="leto", value.name="0-14", na.rm=TRUE)
@@ -98,6 +115,7 @@ data12 <- read_csv2("podatki/T7odrasli.csv", skip=5, n_max=13,
                     locale=locale(encoding="Windows-1250"))
 
 data12 <- data12[,c("regije",2002:2017)]
+data12$regije <- regije
 
 starostne.skupine.regije2 <- melt(data12, id.vars="regije", measure.vars=names(data12)[-1],
                                   variable.name="leto", value.name="15-64", na.rm=TRUE)
@@ -107,7 +125,8 @@ data13 <- read_csv2("podatki/T7starejsi.csv", skip=5, n_max=13,
                     col_names=c("zbris","regije",2002:2017),
                     locale=locale(encoding="Windows-1250"))
 
-data13 <- data12[,c("regije",2002:2017)]
+data13 <- data13[,c("regije",2002:2017)]
+data13$regije <- regije
 
 starostne.skupine.regije3 <- melt(data13, id.vars="regije", measure.vars=names(data13)[-1],
                                   variable.name="leto", value.name="65+", na.rm=TRUE)
--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -1,13 +1,21 @@
 # 3. faza: Vizualizacija podatkov
 
-# Uvozimo zemljevid.
-zemljevid <- uvozi.zemljevid("http://baza.fmf.uni-lj.si/OB.zip", "OB",
-                             pot.zemljevida="OB", encoding="Windows-1250")
-levels(zemljevid$OB_UIME) <- levels(zemljevid$OB_UIME) %>%
-  { gsub("Slovenskih", "Slov.", .) } %>% { gsub("-", " - ", .) }
-zemljevid$OB_UIME <- factor(zemljevid$OB_UIME, levels=levels(obcine$obcina))
-zemljevid <- fortify(zemljevid)
+graf.odpadki.regije.2010 <- ggplot(nastali.odpadki.regije %>% filter(regije != 'SLOVENIJA', leto == 2010)) + 
+  aes(x=regije, y=tone) + geom_col() + 
+  xlab("Statistične regije") + ylab("Nastali odpadki (tone)") +
+  ggtitle("Stolpični diagram nastalh odpadkov v letu 2010 za statistične regije Slovenije") + 
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.3, hjust=1))
 
-# Izračunamo povprečno velikost družine
-povprecja <- druzine %>% group_by(obcina) %>%
-  summarise(povprecje=sum(velikost.druzine * stevilo.druzin) / sum(stevilo.druzin))
+graf.odpadki.regije.2017 <- ggplot(nastali.odpadki.regije %>% filter(regije != 'SLOVENIJA', leto == 2017)) + 
+                                      aes(x=regije, y=tone) + geom_col() + 
+                                      xlab("Statistične regije") + ylab("Nastali odpadki (tone)") + 
+                                      ggtitle("Stolpični diagram nastalh odpadkov v letu 2017 za statistične regije Slovenije") +  
+                                      theme(axis.text.x = element_text(angle = 90, vjust = 0.3, hjust=1))
+
+graf.locevanje.skozi.leta <- ggplot(vrsta.odpadkov %>% filter(vrsta_odpadkov %in% c("1501 EMBALAŽA VKLJUCNO Z LOCENO ZBRANO EMB.", "200101 Papir ter karton in lepenka", "200139 Plastika"))) +
+   aes(x=leto, y=tone, group=vrsta_odpadkov, colour=vrsta_odpadkov) + geom_line()
+
+# Uvozimo zemljevid.
+
+zemljevid <- uvozi.zemljevid("https://biogeo.ucdavis.edu/data/gadm3.6/gpkg/gadm36_SVN_gpkg.zip",
+                             "gadm36_SVN_gpkg", encoding="Windows-1250") %>% fortify()
