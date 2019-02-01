53291576b1f23a7012ffcd4e9c257b56a7ee4c3c
Saša Prelog <sasa.prelog6@gmail.com>
2019-02-01 23:06:45 +0100

    zemljevid

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -70,6 +70,5 @@ graf.investicije.in.locevanje.savinjska <- ggplot(right_join(investicije.regije 
 
 
 # Uvozimo zemljevid.
-
-#zemljevid <- uvozi.zemljevid("https://biogeo.ucdavis.edu/data/gadm3.6/gpkg/gadm36_SVN_gpkg.zip",
-#                             "gadm36_SVN_gpkg", encoding="Windows-1250") %>% fortify()
+zemljevid <- uvozi.zemljevid("http://biogeo.ucdavis.edu/data/gadm2.8/shp/SVN_adm_shp.zip",
+                             "SVN_adm1", mapa = "zemljevid_Slovenije", encoding = "UTF-8") %>% fortify()
