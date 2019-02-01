e67ca23179a8618665afae33af4ebb4b474bd69f
Saša Prelog <sasa.prelog6@gmail.com>
2019-02-01 22:39:13 +0100

    narejen shiny posodobljeno poročilo, narejena analiza (razen zemljevida)

--- analiza/analiza.r
+++ analiza/analiza.r
@@ -1,11 +1,28 @@
 # 4. faza: Analiza podatkov
 
-podatki <- obcine %>% transmute(obcina, povrsina, gostota,
-                                gostota.naselij=naselja/povrsina) %>%
-  left_join(povprecja, by="obcina")
-row.names(podatki) <- podatki$obcina
-podatki$obcina <- NULL
+#tabela za grupiranje
+podatki <- right_join(tabela3[c(1,2,4,6)] %>% filter(leto == 2016), tabela1[c(1,8,9,10)])
+podatki <- right_join(podatki, tabela2 %>% filter(leto == 2016)) %>% filter(regije != "Slovenija")
+podatki$leto <- NULL
+row.names(podatki) <- podatki$regije
+podatki$regije <- NULL
+podatki.norm <- podatki %>% scale()
 
-# Število skupin
-n <- 5
-skupine <- hclust(dist(scale(podatki))) %>% cutree(n)
+# graf za grupiranje v shiny
+
+graf.zemljevid <- function(st){
+  k <- kmeans(podatki.norm, st, nstart=1000)
+  skupina <- data.frame(regije=row.names(podatki.norm), skupina=factor(k$cluster))
+  #ggplot(right_join(zemljevid,skupina, by=c(""="regije"))) + geom_polygon() + 
+  #                    aes(x=long, y=lat, group=group, fill=skupina) + xlab("") + ylab("")
+  #fejk graf ker zemljevid ni uvozen
+  ggplot(right_join(tabela2, loceni.odpadki.regije)) + aes(x=delez_sta , y=delez) + geom_point()
+}
+
+
+#graf za shiny za ločevanje
+graf.regije <- function(regija){
+  ggplot(loceni.odpadki.regije %>% filter(regije %in% regija)) + aes(x=leto, y=delez, group=regije, color=regije) +
+    geom_line() + geom_point()+ xlab("Leto") + ylab("Delež ločenih odpadkov (od nastalih) v %") +
+    ggtitle("Graf deleža ločenih odpadkov")
+}
